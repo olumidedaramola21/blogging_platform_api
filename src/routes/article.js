66bb8f03c48cc4bf726191c00e5cb5aecbfe6107
Olumide Daramola <96479799+olumidedaramola21@users.noreply.github.com>
2025-08-25 08:20:38 +0100

    feat: Add Get /articles with filtering, pagination, and error hhandling

---
+++ src/routes/article.js
@@ -0,0 +1,58 @@
+const Article = require("../models/Article")
+
+// Define all article related routes
+async function articleRoutes(fastify, options) {
+    fastify.get("/articles",  async(request, reply ) => {
+        try {
+            const { tags, author, limit = 10, page = 1 } = request.query
+
+            // convert limit and page to integers for math operation
+            limit = parseInt(limit)
+            page = parseInt(page)
+
+            // Init mongodb filter object based on the query parameters
+            const filter = {isPublished: true}
+            if (tags) {
+                filter.tags = {$in: tags.split(",")}    // support multilpe tags and $in tells mongodb to return artiles with any of the tags
+            }
+            if (author) {
+                filter.author = new RegExp(author, "i")    // case-insensitive search
+            }
+            // calculate pagination
+            const skip = (page - 1) * limit;
+
+            // query database with filter, sort, and pagination
+            const articles = await Article
+                .find(filter)
+                .sort({publishedDate: -1})      // newest first
+                .limit(limit)
+                .skip(skip)               // skip N results for pagination
+                .select("-__v");    // Exclude version field from response
+
+            const total = await Article.countDocuments(filter)
+            return {
+                success: true,
+                data: articles,
+                pagination: {
+                    currentPage: page,
+                    totalPages: Math.ceil(total/limit),
+                    totalArticles: total,
+                    hasNextPage: page < Math.ceil(total/limit),
+                    hasPrevPage: page > 1
+                }
+            }      
+       } catch (error) {
+        reply.code(500).send({
+            success: false,
+            error: "Failed  to retrieve articles",
+            details: error.message
+        });
+
+    }
+    });
+
+ 
+}
+
+
+module.exports = articleRoutes;
